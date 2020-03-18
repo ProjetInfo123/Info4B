c45366315fd3eb2b9cee7d250880dec90b5e3bf0
eguientat <62184912+eguientat@users.noreply.github.com>
2020-03-18 17:33:42 +0100

    bob

---
+++ algorigolo.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import twitter4j.*;
+
+public class algorigolo {
+
+    void algoHash(String debut,String fin,Twitter t,String h){
+      Query q = new Query();
+      q.setSince(debut);
+      q.setUntil(fin);
+      HashtagEntity ht[]=tweet.getHashtagEntities();
+      QueryResult qr = t.search(q);
+      for(int i=0;i<ht.length;i++){
+
+
+
+      }
+
+
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args){
+      Twitter twit = TwitterFactory.getSingleton();
+    	twitter.setOAuthConsumer("3mvRhoAcdYBOdAqARS3XuPwwy", "xhyGCJYDBUkGVO0YG7VkfeBeKj0WYehSyr7P39zpI2FDicv0Pw");
+    	twitter.setOAuthAccessToken("954367945626529798-wbMULzoFfntKnVMcEXv7e3B2IFFB3YP")
+      twitter.setOAuthAccessTokenSecret("4xFQKsiYio3f05TDS4UURQLnSLKrX1F9l71E94YXyGY0O");
+      Date d = new Date(System.currentTimeMillis());
+      Query q = new Query("e");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
