e61627e1aaba3a05d4f3b0e1c289ffb240dba676
Azeddine0 <62209358+Azeddine0@users.noreply.github.com>
2020-03-18 17:52:06 +0100

    t

--- algorigolo.java
+++ algorigolo.java
@@ -7,12 +7,12 @@ public class algorigolo {
       Query q = new Query("e");
       q.setSince(debut);
       q.setUntil(fin);
-      HashtagEntity ht[]=tweet.getHashtagEntities();
+      HashtagEntity ht[]=t.getHashtagEntities();
       QueryResult qr = t.search(q);
       for(Status status = qr.getTweets()){
           for(int i=0;i<ht.length;i++){
                 if(ht[i].getText().equals(h)){
-                  System.out.println("Ce tweet a été écrit par : @"+tweet.getUser().getScreenName());
+                  System.out.println("Ce tweet a été écrit par : @"+t.getUser().getScreenName());
                   System.out.println("Ce tweet date du "+status.getCreatedAt());
                 }
           }
