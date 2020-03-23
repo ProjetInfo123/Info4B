1c67122f6b6006ba7b57a6a3cf0c8682052ea9d3
Azeddine0 <62209358+Azeddine0@users.noreply.github.com>
2020-03-23 12:11:15 +0100

    ftdg

--- descrtw.java
+++ descrtw.java
@@ -28,7 +28,7 @@ public class descrtw {
     public static void main(String[] args) throws TwitterException{
 	 // The factory instance is re-useable and thread safe.
     Twitter twitter = TwitterFactory.getSingleton();
-    Query query = new Query("peripateticienne");
+    Query query = new Query("je souhaite a changer mon binome");
     QueryResult result = twitter.search(query);
 	descrtw d=new descrtw();
     for (Status status : result.getTweets()) {
