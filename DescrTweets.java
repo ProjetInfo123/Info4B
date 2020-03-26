c29d07650c8f15425e7fda4d97067d89f08501a8
Azeddine0 <62209358+Azeddine0@users.noreply.github.com>
2020-03-26 13:06:29 +0100

    f

---
+++ DescrTweets.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import twitter4j.*;
+
+
+public class DescrTweets {
+	private User utilisateur;
+	private Date date;
+	private String message;
+	private HashtagEntity hashtags[];
+	private UserMentionEntity mentions[];
+	private URLEntity url[];
+
+	public DescrTweets(Status tweet){
+		System.out.println();
+		System.out.println("Ce tweet a été écrit par : @"+tweet.getUser().getScreenName());
+		System.out.println("Ce tweet date du : "+tweet.getCreatedAt());
+		System.out.println("Le message du tweet est : "+tweet.getText());
+		HashtagEntity ht[]=tweet.getHashtagEntities();
+		UserMentionEntity ume[]=tweet.getUserMentionEntities();
+		URLEntity ue[]=tweet.getURLEntities();
+		for(int i=0;i<ht.length;i++){
+			System.out.println("Ce tweet contient le hashtag : #"+ht[i].getText());
+		}
+		for(int i=0;i<ume.length;i++){
+			System.out.println("Ce tweet contient la mention d'utilisateur de : @"+ume[i].getScreenName());
+		}
+		for(int i=0;i<ue.length;i++){
+			System.out.println("Ce tweet contient les URL : "+ue[i].getURL());
+		}
+
+		this.utilisateur=tweet.getUser();
+		this.date=tweet.getCreatedAt();
+		this.message=tweet.getText();
+		this.hashtags=tweet.getHashtagEntities();
+		this.mentions=tweet.getUserMentionEntities();
+		this.url=tweet.getURLEntities();
+	}
+
+
+	public class Indexation extends Thread {//ajouter des hashtables
+		private LinkedList<Status> fa;
+		private DescrTweets d;
+
+		public Indexation(LinkedList<Status> fa,DescrTweets d){
+								this.fa=fa;
+								this.d=d;
+		}
+
+		public void run(){
+				while(fa.size()>0){
+					Status tweet=fa.removeFirst();
+
+
+
+
+
+				}
+
+
+
+		}
+
+	}
+
+
+
+
+    public static void main(String[] args) throws TwitterException{
+	 // The factory instance is re-useable and thread safe.
+	  LinkedList<Status> fa=new LinkedList<>();
+    Twitter twitter = TwitterFactory.getSingleton();
+    Query query = new Query("je binome");
+    QueryResult result = twitter.search(query);
+		DescrTweets d;
+    for (Status status : result.getTweets()) {
+        d=new DescrTweets(status);
+				fa.addLast(status);
+    }
+		System.out.println(fa.size());
+    }
+}
--- algorigolo.java
+++ algorigolo.java
@@ -1,26 +1,23 @@
-import java.util.List;
+import java.util.*;
 import twitter4j.*;
-import java.util.Date;
 
 public class algorigolo {
 
     public void algoHash(String debut,String fin,String h) throws TwitterException{
       TwitterFactory tf = new TwitterFactory();   //utiliser TwitterStream, TwitterListener et FilterQuery
       Twitter tweet = tf.getInstance();
-      Query q = new Query("doom");
+      Query q = new Query("changer binome");
       q.setSince(debut);
       q.setUntil(fin);
+      q.setCount(100);
       QueryResult qr = tweet.search(q);
-      int x=0;
       for(Status status : qr.getTweets()){
         HashtagEntity ht[]=status.getHashtagEntities();
-        x++;
           for(int i=0;i<ht.length;i++){
                   System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
                   System.out.println("Ce tweet date du "+status.getCreatedAt());
 
           }
-          System.out.println("Il y a "+x+" tweets");
       }
     }
 
@@ -34,6 +31,6 @@ public class algorigolo {
     public static void main(String[] args) throws TwitterException{
       Twitter twitter = TwitterFactory.getSingleton();
       algorigolo a=new algorigolo();
-      a.algoHash("2020-03-21","2020-03-22","DOOMEternal");
+      a.algoHash("2020-03-21","2020-03-22","");
     }
 }
